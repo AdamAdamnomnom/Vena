3009b16feaad886e2f2337859a5910f2951a7aa1
AdamAdamnomnom <115740020+AdamAdamnomnom@users.noreply.github.com>
2023-09-03 02:56:20 +0200

    update and new line to test

--- main.ino
+++ main.ino
@@ -1,13 +1,21 @@
 #include "Arduino.h"
 #include "nzStero.h"
 
+
 nzStero myStero;
 
 void setup() {
     Serial.begin(9600);
     //myStero.addExtraVar(int M4, int 0)
     myStero.initialize(); // Inicjalizacja biblioteki
-    myStero.addExtraVar("M1", 0);
+     myStero.addExtraVar("M1", 1);
+     myStero.addExtraVar("M2", 0);
+     myStero.addExtraVar("M3", 1);
+     myStero.addExtraVar("M4", 0);
+     myStero.addExtraVar("M5", 1);
+     myStero.addExtraVar("M6", 0);
+     Serial.println("incijalizeded udanet");
+    // myStero.addExtraVar("M3", 1);
 }
 
 void loop() {
@@ -27,10 +35,30 @@ void loop() {
     myStero.out(3, input3);
 
     // Wyświetlenie wyników na monitorze szeregowym
-    myStero.showInOut(1,1);
-   // Serial.print(" AND Result: ");
-    //Serial.println(andResult);
-    myStero.setExtraVar("M1", 1);
+    //myStero.showInOut(1,1);
+  //  // Serial.print(" AND Result: ");
+    Serial.print("marker 1 ");
     Serial.println(myStero.getExtraVar("M1"));
+    Serial.print("marker 2 ");
+    Serial.println(myStero.getExtraVar("M2"));
+    Serial.print("marker 3 ");
+    Serial.println(myStero.getExtraVar("M3"));
+    Serial.print("marker 4 ");
+    Serial.println(myStero.getExtraVar("M4"));
+    Serial.print("marker 5 ");
+    Serial.println(myStero.getExtraVar("M5"));
+    Serial.print("marker 6 ");
+    Serial.println(myStero.getExtraVar("M6"));
+// Serial.println("Pierwsze 20 adresy i dane w pamięci EEPROM:");
+//       for (int i = 0; i < 20; i++) {
+//         int odczytanaWartosc = EEPROM.read(i);
+//         Serial.print("Adres ");
+//         Serial.print(i);
+//         Serial.print(": ");
+//         Serial.println(odczytanaWartosc);
+//       }
     delay(400); // Odstęp czasowy
+    while(true){
+
+    };
 }
